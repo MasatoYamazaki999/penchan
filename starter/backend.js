b801a942faa6bb459b58c0f2b7416ad952bca541
= <=>
2024-05-22 20:09:14 +0900

    work

--- starter/backend.js
+++ starter/backend.js
@@ -5,7 +5,7 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
-const io = new Server(server)
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
 
 const port = 3000
 
@@ -15,17 +15,24 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-const players = {}
+const backEndPlayers = {}
 
 io.on('connection', (socket) => {
   console.log('a user connected')
-  players[socket.id] = {
-    x: 100,
-    y: 100
+  backEndPlayers[socket.id] = {
+    x: 500 * Math.random(),
+    y: 500 * Math.random()
   }
-  io.emit('updatePlayers', players)
 
-  console.log(players)
+  io.emit('updatePlayers', backEndPlayers)
+
+  socket.on('disconnect', (reason) => {
+    console.log(reason)
+    delete backEndPlayers[socket.id]
+    io.emit('updatePlayers', backEndPlayers)
+  })
+
+  console.log(backEndPlayers)
 })
 
 server.listen(port, () => {
