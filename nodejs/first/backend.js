d8aa51f099df59d8f411a8b45095e855daa27719
= <=>
2024-05-25 01:23:20 +0900

    3:02:32

--- nodejs/first/backend.js
+++ nodejs/first/backend.js
@@ -1,69 +1,96 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 
 // socket.io setup
-const http = require('http')
+const http = require("http")
 const server = http.createServer(app)
-const { Server } = require('socket.io')
+const { Server } = require("socket.io")
 const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
 
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static("public"))
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html")
 })
 
 const backEndPlayers = {}
+const backEndProjectiles = {}
 
 const SPEED = 10
+let projectileId = 0
 
-io.on('connection', (socket) => {
-  console.log('a user connected')
+io.on("connection", (socket) => {
+  console.log("a user connected")
   backEndPlayers[socket.id] = {
     x: 500 * Math.random(),
     y: 500 * Math.random(),
     color: `hsl(${360 * Math.random()}, 100%, 50%)`,
-    sequenceNumber: 0
+    sequenceNumber: 0,
   }
-  
-  io.emit('updatePlayers', backEndPlayers)
 
-  socket.on('disconnect', (reason) => {
-    console.log('disconnect reason: '+reason)
-    delete backEndPlayers[socket.id]
-    io.emit('updatePlayers', backEndPlayers)
+  io.emit("updatePlayers", backEndPlayers)
+
+  socket.on("shoot", ({ x, y, angle }) => {
+    projectileId++
+
+    const velocity = {
+      x: Math.cos(angle) * 5,
+      y: Math.sin(angle) * 5,
+    }
+
+    backEndProjectiles[projectileId] = {
+      x,
+      y,
+      velocity,
+      playerId: socket.id
+    }
+
+    console.log(backEndProjectiles)
   })
 
-  socket.on('keydown', ({ keycode, sequenceNumber }) => {
+  socket.on("disconnect", (reason) => {
+    console.log("disconnect reason: " + reason)
+    delete backEndPlayers[socket.id]
+    io.emit("updatePlayers", backEndPlayers)
+  })
+
+  socket.on("keydown", ({ keycode, sequenceNumber }) => {
     backEndPlayers[socket.id].sequenceNumber = sequenceNumber
-    switch(keycode) {
-      case 'KeyW':
+    switch (keycode) {
+      case "KeyW":
         backEndPlayers[socket.id].y -= SPEED
         break
-  
-      case 'KeyA':
+
+      case "KeyA":
         backEndPlayers[socket.id].x -= SPEED
         break
-  
-      case 'KeyS':
+
+      case "KeyS":
         backEndPlayers[socket.id].y += SPEED
         break
-  
-      case 'KeyD':
+
+      case "KeyD":
         backEndPlayers[socket.id].x += SPEED
         break
-      }
+    }
   })
 })
 
+// backend ticker
 setInterval(() => {
-  io.emit('updatePlayers', backEndPlayers)
+  // update projectile position
+  for (const id in backEndProjectiles) {
+    backEndProjectiles[id].x += backEndProjectiles[id].velocity.x
+    backEndProjectiles[id].y += backEndProjectiles[id].velocity.y
+  }
+  io.emit("updateProjectiles", backEndProjectiles)
+  io.emit("updatePlayers", backEndPlayers)
 }, 15)
 
 server.listen(port, () => {
   console.log(`Im listening on port ${port}`)
 })
 
-console.log('server did load')
+console.log("server did load")
