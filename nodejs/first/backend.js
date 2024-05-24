0d6ecfe6bed31ee166fc2796ec6cd711929f9ab8
= <=>
2024-05-24 20:36:06 +0900

    1:52:27

--- nodejs/first/backend.js
+++ nodejs/first/backend.js
@@ -17,6 +17,8 @@ app.get('/', (req, res) => {
 
 const backEndPlayers = {}
 
+const SPEED = 10
+
 io.on('connection', (socket) => {
   console.log('a user connected')
   backEndPlayers[socket.id] = {
@@ -36,19 +38,19 @@ io.on('connection', (socket) => {
   socket.on('keydown', (keycode) => {
     switch(keycode) {
       case 'KeyW':
-        backEndPlayers[socket.id].y -= 5
+        backEndPlayers[socket.id].y -= SPEED
         break
   
       case 'KeyA':
-        backEndPlayers[socket.id].x -= 5
+        backEndPlayers[socket.id].x -= SPEED
         break
   
       case 'KeyS':
-        backEndPlayers[socket.id].y += 5
+        backEndPlayers[socket.id].y += SPEED
         break
   
       case 'KeyD':
-        backEndPlayers[socket.id].x += 5
+        backEndPlayers[socket.id].x += SPEED
         break
       }
   })
