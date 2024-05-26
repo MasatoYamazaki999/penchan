80df875417b9266a72d18111750c8fde5f35f925
= <=>
2024-05-27 00:35:17 +0900

    HitAndBlow

---
+++ nodejs/hit_and_blow/backend.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+const http = require('http')
+const server = http.createServer(app)
+
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+server.listen(port, () => {
+  console.log('lisen')
+})
+
+console.log('frontend.js started.')
---
+++ nodejs/hit_and_blow/public/js/classes/HitAndBlow.js
@@ -0,0 +1,12 @@
+class HitAndBlow {
+  constructor() {
+  }
+
+  hit() {
+    console.log('hit')
+  }
+
+  blow() {
+    console.log('blow')
+  }
+}
