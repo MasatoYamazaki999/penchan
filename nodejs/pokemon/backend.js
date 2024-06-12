de63cf152b0047c1aee838f2af69d5024707485d
Masato Yamazaki <yamazaki@silic.co.jp>
2024-06-12 14:15:20 +0900

    p

--- nodejs/pokemon/backend.js
+++ nodejs/pokemon/backend.js
@@ -41,7 +41,6 @@ io.on('connection', (socket) => {
   })
   
   socket.on('updateWorld', (world) => {
-    //console.log(world)
     backEndPlayers[socket.id].world = world
   })
 
@@ -57,7 +56,7 @@ io.on('connection', (socket) => {
 
 setInterval(() => {
   io.emit('updatePlayers', backEndPlayers, sockets)
-}, 15)
+}, 20)
 
 server.listen(port, () => {
   console.log(`Im listening on port ${port}`)
