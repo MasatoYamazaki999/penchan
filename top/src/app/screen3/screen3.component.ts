56cca345fe634d7663a5de6d48d1f7dcc890af91
MasatoYamazaki999 <yamazaki@silic.co.jp>
2024-08-01 07:57:33 +0900

    test

--- top/src/app/screen3/screen3.component.ts
+++ top/src/app/screen3/screen3.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Comp0Component } from "../comp0/comp0.component";
+import { Comp1Component } from "../comp1/comp1.component";
+import { Comp2Component } from "../comp2/comp2.component";
 
 @Component({
   selector: 'app-screen3',
   standalone: true,
-  imports: [],
+  imports: [Comp0Component, Comp1Component, Comp2Component],
   templateUrl: './screen3.component.html',
   styleUrl: './screen3.component.css'
 })
